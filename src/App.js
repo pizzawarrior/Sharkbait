d3db6cd0ed15f57ab4c1927e5b4bd11a6494590c
pizzawarrior <i.norstad@gmail.com>
2022-12-12 12:33:25 -0800

    cleanup on aisle 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import USA_States_by_Incidents_1959_2018 from './Assets/2. USA States by Incidents 1959-2018.png'
-import CA_Incidents_by_County from './Assets/5_All CA Incidents by County Percentage.png'
-//import CA_Species_by_County from './Assets/17. Species and percentages by county.png'
-//import CA_Northern_Southern_Heatmap from './Assets/13_CA days of shark attacks_4.png'
 import "@progress/kendo-theme-material/dist/all.css";
 import "./App.css";
 import { TileLayout } from "@progress/kendo-react-layout";
 import { useState } from "react";
 import "./App.css";
 import StaticImage from "./components/StaticImage";
-//import { format } from '@progress/kendo-intl';
 import IncidentForm from "./components/IncidentForm";
 import MostRecent from "./components/MostRecent";
 import TopLocations from "./components/TopLocations";
-
-//import Map from "./components/Map"
 
 
 const initialPositions = [
@@ -56,11 +50,10 @@ function App() {
 const widgets = [
   
   {
-    // header: "Sharktacular",
      body: <StaticImage 
      imageSource ={USA_States_by_Incidents_1959_2018}
      imageAlt = "USA Map of Incidents"
-     />,
+     />
    },
   
    {
@@ -80,14 +73,13 @@ const widgets = [
 
 {
     body: <MostRecent
-    //body: <Map
     />
 },
 
   {
     header: "Report A New Incident",
     body: <IncidentForm
-  />,
+  />
   }
 ];
 
